5d6b14c854adc9020a83e7aa37f6cc61f38e81f2
mohit-soni2003 <mohitsonip1847@gmail.com>
2025-01-29 22:58:01 +0530

    -

--- server/utils/generateTokenAndSetCookie.js
+++ server/utils/generateTokenAndSetCookie.js
@@ -4,7 +4,7 @@ const generateTokenAndSetCookie =(res,userId)=>{
     const token = jwt.sign({userId}, jwt_secret ,{ expiresIn:"7d"});
     res.cookie("token", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,  // Cookie expires in 7 days
-        httpOnly: true,  // Ensure the cookie is only accessible via HTTP requests, not client-side JS
+        httpOnly: false,  // Ensure the cookie is only accessible via HTTP requests, not client-side JS
         secure: true,  // Set to false for development over HTTP
         sameSite: 'None',  // Helps with cross-origin requests while keeping some protection
     });
