9b160d8461d628b6cb9d4b8653ccd7b8c9f305de
mohit-soni2003 <mohitsonip1847@gmail.com>
2025-01-29 22:20:02 +0530

    -

--- server/keys.js
+++ server/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     jwt_secret : "Mohit1234",
-    FRONTEND_ORIGIN_URL:"http://localhost:5173",
+    FRONTEND_ORIGIN_URL:"https://rane-project.vercel.app",
     MONGO_URL:"mongodb+srv://mohit:Mohit123@cluster0.ruqg5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 }
