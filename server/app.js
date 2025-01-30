76a9f553ceafe5477856444201af82d355cbccfc
mohit-soni2003 <mohitsonip1847@gmail.com>
2025-01-30 23:59:55 +0530

    -

--- server/app.js
+++ server/app.js
@@ -14,6 +14,16 @@ const corsOptions = {
   credentials: true,  // Allow cookies to be sent
 };
 
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://rane-project.vercel.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+
 // Apply CORS middleware globally
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));  // Handle OPTIONS preflight requests
