ae605f09a976da89bb2dadd991c6fc8fe28fa2f1
mohit-soni2003 <mohitsonip1847@gmail.com>
2025-02-18 18:18:01 +0530

    Cors Policy error resolved

--- client/rane_infra/src/assets/components/store/keyStore.js
+++ client/rane_infra/src/assets/components/store/keyStore.js
@@ -1,4 +1,4 @@
-const backend_url = "http://localhost:3000"
+const backend_url = "https://rane-project-server.vercel.app"
 const UPLOAD_PRESET= "rane_infra1"
 const CLOUD_NAME= "drl3qqrq3"//mohit
 const CLOUDINARY_URL_IMAGE = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`
@@ -6,6 +6,6 @@ const CLOUDINARY_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/raw/upload
 
 
 export {backend_url , CLOUDINARY_URL,UPLOAD_PRESET,CLOUD_NAME,CLOUDINARY_URL_IMAGE}
-// const backend_url = "https://rane-project-server.vercel.app"
+// const backend_url = "http://localhost:3000"
 // const UPLOAD_PRESET= "rane_infra"
 // const CLOUD_NAME= "mohitcloud2003"
