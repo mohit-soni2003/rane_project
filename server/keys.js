b9017dc66ff314f949733aeaa6232e0a68ab8404
mohit-soni2003 <mohitsonip1847@gmail.com>
2025-02-22 00:14:46 +0530

    Keys updated

--- server/keys.js
+++ server/keys.js
@@ -1,15 +1,15 @@
 module.exports = {
     
-    FRONTEND_ORIGIN_URL:"http://localhost:5173",
-    SENDER_MAIL:"mohitsonip1847@gmail.com",
-    MONGO_URL:"mongodb+srv://mohit:Mohit123@cluster0.ruqg5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    MAIL_PASS:"hlyq uoeg rcyr sxtd",//mohit
+    // FRONTEND_ORIGIN_URL:"http://localhost:5173",
+    // SENDER_MAIL:"mohitsonip1847@gmail.com",
+    // MONGO_URL:"mongodb+srv://mohit:Mohit123@cluster0.ruqg5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    // MAIL_PASS:"hlyq uoeg rcyr sxtd",//mohit
     
     
-    // FRONTEND_ORIGIN_URL:"https://www.raneandsons.in",
-    // SENDER_MAIL:"ranesons11@gmail.com",
-    // MAIL_PASS:"peli uazp ttpj ejya",//rane
-    // MONGO_URL:"mongodb+srv://ranesons11:SEycv5RPk65Y1xOY@cluster0.et6jd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    
+    FRONTEND_ORIGIN_URL:"https://www.raneandsons.in",
+    SENDER_MAIL:"ranesons11@gmail.com",
+    MAIL_PASS:"peli uazp ttpj ejya",//rane
+    MONGO_URL:"mongodb+srv://ranesons11:SEycv5RPk65Y1xOY@cluster0.et6jd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+
     jwt_secret : "Mohit1234",
 }
