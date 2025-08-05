f75e323e6d1dc94a17d372f1c237bc77455ff55a
mohit-soni2003 <mohitsonip1847@gmail.com>
2025-08-05 15:47:11 +0530

    test deployment of version 3 on server

--- client/rane_infra/src/store/keyStore.js
+++ client/rane_infra/src/store/keyStore.js
@@ -1,4 +1,4 @@
-const backend_url = "https://rane-project-server.vercel.app"
+// const backend_url = "https://rane-project-server.vercel.app"
 
 // const UPLOAD_PRESET= "rane_infra1"
 // const CLOUD_NAME= "drl3qqrq3"//rane
@@ -9,8 +9,7 @@ const UPLOAD_PRESET= "rane_infra"
 const CLOUD_NAME= "mohitcloud2003"
 
 
-// const backend_url = "https://rane-project-server-git-version3o-mohit-sonis-projects-6fb4d728.vercel.app"
-// const backend_url = "https://rane-project-server-git-version3o-mohit-sonis-projects-6fb4d728.vercel.app/" //version 3.o
+const backend_url = "https://rane-project-server-git-version3o-mohit-sonis-projects-6fb4d728.vercel.app" //version 3.o
  
 const CLOUDINARY_URL_IMAGE = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`
 const CLOUDINARY_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/raw/upload`
