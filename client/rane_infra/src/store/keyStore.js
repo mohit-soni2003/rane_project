8dfda56ab71a0007944185f93e98831aac44fd6f
mohit-soni2003 <mohitsonip1847@gmail.com>
2025-09-14 13:51:21 +0530

    Keys updated

--- client/rane_infra/src/store/keyStore.js
+++ client/rane_infra/src/store/keyStore.js
@@ -1,10 +1,10 @@
-// const backend_url = "https://rane-project-server.vercel.app"
+const backend_url = "https://rane-project-server.vercel.app"
 
 const UPLOAD_PRESET= "rane_infra1"
 const CLOUD_NAME= "drl3qqrq3"//rane
 
 
-const backend_url = "http://localhost:3000"
+// const backend_url = "http://localhost:3000"
 
 // const UPLOAD_PRESET= "rane_infra"
 // const CLOUD_NAME= "mohitcloud2003" 
