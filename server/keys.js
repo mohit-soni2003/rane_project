5bbb9592fcfd064cd7305128990b55a01f7cf1e7
mohit-soni2003 <mohitsonip1847@gmail.com>
2025-09-15 21:17:38 +0530

    UI changes of dashboard of client , admin , staff done

--- server/keys.js
+++ server/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    FRONTEND_ORIGIN_URL:"http://localhost:5173",
+    // FRONTEND_ORIGIN_URL:"http://localhost:5173",
     // SENDER_MAIL: "mohitsonip1847@gmail.com",
     // MAIL_PASS: "hlyq uoeg rcyr sxtd",//mohit
     // MONGO_URL:"mongodb+srv://mohit:Mohit123@cluster0.ruqg5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
@@ -9,7 +9,7 @@ module.exports = {
 
  
 
-    // FRONTEND_ORIGIN_URL:"https://www.raneandsons.in",
+    FRONTEND_ORIGIN_URL:"https://www.raneandsons.in",
 
     MONGO_URL:"mongodb+srv://ranesons11:SEycv5RPk65Y1xOY@cluster0.et6jd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     MAIL_PASS:"acby zvfn pury saoa",//rane  SEycv5RPk65Y1xOY
